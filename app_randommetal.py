6c4f5a9f97acfb7833a9ad808ecc040535bde3c8
pephorror666 <jpulidoc@proton.me>
2025-02-09 03:54:16 +0100

    Update app_randommetal.py

--- app_randommetal.py
+++ app_randommetal.py
@@ -141,7 +141,8 @@ def format_genres_as_links(genres):
 #Get the genre filter from the URL
 #url_params = st.experimental_get_query_params()  ### This line is the one that works locally the next one for the online app in streamlit community cloud
 url_params = st.get_query_params
-genre_filter_from_url = url_params.get("genre_filter", None)
+#genre_filter_from_url = url_params.get("genre_filter", None)
+genre_filter_from_url = query_params.get("genre_filter", None)
 if genre_filter_from_url:
     st.session_state.genre_filter = genre_filter_from_url[0]
     st.session_state.shown_artists = []
